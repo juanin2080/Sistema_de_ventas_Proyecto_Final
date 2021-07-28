f004ecf0bbaef11ee31ea75d76e116ef2c44574b
juanin2080 <juancar208093@gmail.com>
2021-07-28 17:11:12 -0500

    Creación controlador BD
    28/07/2021

--- src/controlador/Conexion.java
+++ src/controlador/Conexion.java
@@ -9,58 +9,27 @@ package controlador;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.swing.JOptionPane;
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 /**
  *
  * @author juana
  */
 public class Conexion {
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Kelly_25092001";
-    private static final String URL = "jdbc:mysql://localhost:3306/proyectods?zeroDateTimeBehavior=CONVERT_TO_NULL";
-    private Connection CN;
+
     private static EntityManagerFactory emf;
     private static EntityManager em;
-    
+
     private static void setup() {
         if (em == null) {
             Conexion.emf = Persistence.createEntityManagerFactory("Sistema_de_Ventas_Proyecto_FinalPU");
             Conexion.em = Conexion.emf.createEntityManager();
         }
     }
-     public Conexion() {
+
+    public Conexion() {
         this.setup();
-        CN = null;
     }
-    
-    /**
-     * Establece la conexion con la bd
-     * @return Conexion
-     */
-    public Connection getConnection(){
-        try {
-            Class.forName(DRIVER);
-            CN = DriverManager.getConnection(URL, USER, PASSWORD);                             
-        } catch (ClassNotFoundException | SQLException ex) {
-            JOptionPane.showMessageDialog(null, ex.getMessage(), "Error al conectar con la bd", JOptionPane.ERROR_MESSAGE);
-            System.exit(0);
-        }
-        return CN;
-    }
-    
-    public void Close(){
-        try {
-            CN.close();
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, ex.getMessage(), "Error al conectar con la bd", JOptionPane.ERROR_MESSAGE);
-        }
-    }
-    
+
     public static EntityManagerFactory getEmf() {
         return emf;
     }
@@ -76,8 +45,6 @@ public class Conexion {
     public static void setEm(EntityManager em) {
         Conexion.em = em;
     }
-
-   
 
     public static void main(String[] args) {
         new Conexion();
